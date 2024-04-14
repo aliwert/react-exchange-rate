d309d789d85fc254a760fc392b8c72e0dddb5335
aliwert <154356044+aliwert@users.noreply.github.com>
2024-04-14 23:29:12 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Currency from './components/Currency';
+
+function App() {
+  return (
+    <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '80vh' }}>
+      <Currency />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Currency.jsx
@@ -0,0 +1,38 @@
+import "../css/currency.css";
+import { FaArrowAltCircleRight } from "react-icons/fa";
+const Currency = () => {
+  return (
+    <div className="currency-div">
+      <div style={{ marginBottom: "30px", fontFamily: "cursive" }}>
+        <h2>Exchange Rate Application</h2>
+      </div>
+      <div style={{ marginTop: "30px" }}>
+        <input type="number" className="amount" />
+        <select className="from-currency-option" name="" id="">
+          <option value="">USD</option>
+          <option value="">EUR</option>
+          <option value="">TL</option>
+        </select>
+        <FaArrowAltCircleRight
+          style={{
+            fontSize: "20px",
+            color: "red",
+            marginRight: "10px",
+            marginTop: "5px",
+          }}
+        />
+        <select className="to-currency-option" name="" id="">
+          <option value="">TL</option>
+          <option value="">USD</option>
+          <option value="">EUR</option>
+        </select>
+        <input type="number" className="result" />
+      </div>
+      <div>
+        <button className="exchange-button">Convert</button>
+      </div>
+    </div>
+  );
+};
+
+export default Currency;
