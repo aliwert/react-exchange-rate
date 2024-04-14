6b2518f5d10ea8854e6e5b2f7b55330064b8af83
aliwert <154356044+aliwert@users.noreply.github.com>
2024-04-14 23:40:16 +0300

    states

--- src/components/Currency.jsx
+++ src/components/Currency.jsx
@@ -1,13 +1,19 @@
 import "../css/currency.css";
+import { useState } from "react";
 import { FaArrowAltCircleRight } from "react-icons/fa";
 const Currency = () => {
+  const  [amount, setAmount] = useState(0)
+  const [fromCurrency, setFromCurrency] = useState("")
+  const [toCurrency, setToCurrency] = useState("")
+  const [result, setResult] = useState(0)
+
   return (
     <div className="currency-div">
       <div style={{ marginBottom: "30px", fontFamily: "cursive" }}>
         <h2>Exchange Rate Application</h2>
       </div>
       <div style={{ marginTop: "30px" }}>
-        <input type="number" className="amount" />
+        <input value={amount} onChange={(e) => setAmount(e.target.value)} type="number" className="amount" />
         <select className="from-currency-option" name="" id="">
           <option value="">USD</option>
           <option value="">EUR</option>
